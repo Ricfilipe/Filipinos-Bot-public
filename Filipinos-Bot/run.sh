5611547c9975ecdb5024e2ea30c83598a225fdbe
Ricfilipe <ricardo.l.filipe@tecnico.ulisboa.pt>
2022-11-26 17:17:19 +0000

    Add missing options

--- Filipinos-Bot/run.sh
+++ Filipinos-Bot/run.sh
@@ -1,5 +1,10 @@
 DISCORD_TOKEN=$(bashio::config 'discord_token')
-MONGO=$(bashio::config 'mongodb_token')
-echo -e "APP_NAME=Filipinos Bot\nTOKEN_MONGODB_GAMBLE=$MONGO\nTOKEN=$DISCORD_TOKEN\n" > /.env
+MONGO_USER=$(bashio::config 'mongo_user')
+MONGO_PASS=$(bashio::config 'mongo_pass')
+GITHUB_TOKEN=$(bashio::config 'github_token')
+TRAKT_API=$(bashio::config 'trakt_api_key')
+WATCH_PARTY=$(bashio::config 'watch_party_api')
+
+echo -e "APP_NAME=Filipinos Bot\nMONGO_USER=$MONGO_USER\nMONGO_PASS=$MONGO_PASS\nTOKEN_GITHUB=$GITHUB_TOKEN\nTRAKT_API_KEY=$TRAKT_API\nTOKEN=$DISCORD_TOKEN\nWATCH_PARTY_API=$WATCH_PARTY" > /.env
 
 node index.js
