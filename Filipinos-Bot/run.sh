018e0d548988d1ee56725ede3038540c44a27c52
Ricfilipe <ricardo.l.filipe@tecnico.ulisboa.pt>
2022-11-26 21:42:51 +0000

    missing script start

--- Filipinos-Bot/run.sh
+++ Filipinos-Bot/run.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/with-contenv bashio
+set -e
+CONFIG="/etc/dhcpd.conf"
+LEASES="/data/dhcpd.lease"
+
+bashio::log.info "Loading Environment..."
+
 DISCORD_TOKEN=$(bashio::config 'discord_token')
 MONGO_USER=$(bashio::config 'mongo_user')
 MONGO_PASS=$(bashio::config 'mongo_pass')
@@ -7,4 +14,5 @@ WATCH_PARTY=$(bashio::config 'watch_party_api')
 
 echo -e "APP_NAME=Filipinos Bot\nMONGO_USER=$MONGO_USER\nMONGO_PASS=$MONGO_PASS\nTOKEN_GITHUB=$GITHUB_TOKEN\nTRAKT_API_KEY=$TRAKT_API\nTOKEN=$DISCORD_TOKEN\nWATCH_PARTY_API=$WATCH_PARTY" > /.env
 
+bashio::log.info "Starting Bot..."
 node index.js
